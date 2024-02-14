f70537cc82f96ba60306c2210460e51fa792366a
Mayur <mayur@groots.in>
2024-02-14 17:05:21 +0000

    day14

--- my-demo/day14/jira-simple-project/simple-list.py
+++ my-demo/day14/jira-simple-project/simple-list.py
@@ -9,7 +9,10 @@ import json
 
 url = "https://groots-mayur.atlassian.net/rest/api/3/project"
 
-auth = HTTPBasicAuth("mayur@groots.in", "ATATT3xFfGF0xp__yIHK4Cj7j1vJQsTTJWAmvrPx8uXj60edzsfYCWE9tNjR3YvUqfNRcn-_HrbhEAs-mcJ_rhV0ID2wORhJAvkb4VRy8okegGm71Tnkxd-3dAP3DfsmrzLGz7Z1e7tspoeZ4bx6Beyb32VKKSptR7jxw6tM-GRUO6lijMDoaVY=9585F3E")
+API_TOKEN="<token here>"
+auth = HTTPBasicAuth("<email here>", API_TOKEN)
+#auth = HTTPBasicAuth("mayur@groots.in", "ATATT3xFfGF0ySEwZuMm9XcPLbPst2N0J88YWOLY49qcYLPbgfnfpsi7bnM8lDc8Pf1bkFV6WxROADQveSZkLQK5EeTvlKgRYi79CqjyJdauCmba8OlzMtxE2TnCthvXmhiBcxHz-N8bLLj3qyGnHRhqW-VNEFWFcMepxdrTKWAt7Qe9OugSHAs=5D1115E7")
+
 
 headers = {
   "Accept": "application/json"
@@ -21,13 +24,8 @@ response = requests.request(
    headers=headers,
    auth=auth
 )
-
 #print(json.dumps(json.loads(response.text), sort_keys=True, indent=4, separators=(",", ": ")))
-#get specific output 
 output = json.loads(response.text)
 
-print(output)
 name = output[0]["name"]
-
 print(name)
-
